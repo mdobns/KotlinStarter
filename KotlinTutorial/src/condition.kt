559cb9df086f317c21f7649856bb6559e8f685f4
Md Oshama Bin Nur <mdobns@hotmail.com>
2025-08-26 16:44:01 +0600

    strings , if else , inputs are added

--- KotlinTutorial/src/condition.kt
+++ KotlinTutorial/src/condition.kt
@@ -10,5 +10,16 @@ fun main(){
     else
         println("$str is not palindrome")
 
+    print("Enter your age:")
+    var age = readln().toInt()
+
+    if (65<age)
+        println("You're really really old")
+    else if (age in 19..65 ) {
+        println("You're an adult")
+    }
+    else
+        println("You're not an adult")
+
 
 }
